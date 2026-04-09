93eb84cd01673fdf3fee91e58f2f9fd2a9d53236
Anthony Soria <anthonysoria659@gmail.net>
2026-04-09 10:00:18 -0500

    Version 3

--- if2.cpp
+++ if2.cpp
@@ -18,12 +18,19 @@ scanf("%f",&p3);
 pf = promedio(p1,p2,p3);
 printf("promedio total =%f\n",pf);
 
-
-return 0;
-
+if(pf<11){
+    printf("Resultado: Reprueba");
 }
-
-float promedio (float n1, float n2, float n3){
-float resultado;
-resultado = (n1+n2+n3) /3 ;
-return resultado;
+else if (pf>=11 && pf<=13.99){
+    printf("Resultado: Supletorio");
+}
+else if (pf>14){
+    printf("Resultado: Aprueba");
+}
+return 0;
+}
+    float promedio (float n1, float n2, float n3){
+        float resultado;
+        reultado = (n1+n2+n3)/3;
+        return resultado;
+    }
